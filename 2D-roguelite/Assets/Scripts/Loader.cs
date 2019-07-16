a0f411fb279f035efbc6a4d3d0d62af339284bc6
Soleyu <soleyu.alpha@gmaill.com>
2019-07-16 16:45:51 -0500

    Added Board and Game Scripts
    Added GameManager Script and Board Manager script

---
+++ 2D-roguelite/Assets/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    //public GameObject soundManager;
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+
+        /* if (soundManager.instance == null)
+        {
+            Instantiate(soundManager);
+        }
+        */
+    }
+}
