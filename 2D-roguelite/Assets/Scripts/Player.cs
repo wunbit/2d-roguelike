5519366aa470e9f2c6e021c0aa2473a2b57c376a
Soleyu <soleyu.alpha@gmaill.com>
2019-07-19 00:24:03 -0500

    updated player script

--- 2D-roguelite/Assets/Scripts/Player.cs
+++ 2D-roguelite/Assets/Scripts/Player.cs
@@ -12,18 +12,23 @@ public class Player : MonoBehaviour
 
     private Animator animator;
     private int food;
-    
+
     // Start is called before the first frame update
     protected override void Start()
     {
         animator = GetComponent<Animator>();
         food = GameManager.instance.playerFoodPoints;
-        base.Start ();
+        base.Start();
     }
 
-     private void OnDisable ()
+    private void OnDisable ()
     {
-        if(!GameManager.instance.playersTurn) 
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn)
         {
             return;
         }
@@ -40,27 +45,14 @@ public class Player : MonoBehaviour
             AttemptMove<Wall> (horizontal, vertical);
         }
     }
-
     protected override void AttemptMove <T> (int xDir, int yDir)
     {
         food--;
         base.AttemptMove <T> (xDir, yDir)
         RaycastHit2D hit;
-        if (Move (xDir, yDir, out hit)) 
-        {
-
-        }
         CheckIfGameOver ();
         GameManager.instance.playersTurn = false;
     }
-
-    protected override void OnCantMove <T> (T component)
-    {
-        Wall hitWall = component as Wall;
-        hitWall.DamageWall (wallDamage);
-        animator.SetTrigger ("playerChop");
-    }
-
     private void OnTriggerEnter2D (Collider2D other)
     {
         if(other.tag == "Exit")
@@ -79,13 +71,18 @@ public class Player : MonoBehaviour
             other.gameObject.SetActive (false);
         }
     }
-
+    protected override void OnCantMove <T> (T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall (wallDamage);
+        animator.SetTrigger ("playerChop");
+    }
     private void Restart ()
     {
-         SceneManager.LoadScene (0);
+        SceneManager.LoadScene (0);
     }
 
-    public void LoseFood (int loss)
+        public void LoseFood (int loss)
     {
         animator.SetTrigger ("playerHit");
         food -= loss;
@@ -96,7 +93,7 @@ public class Player : MonoBehaviour
     {
         if (food <= 0)
         {
-            GameManager.instance.GameOver (); 
+            GameManager.instance.GameOver ();
         }
     }
 }
